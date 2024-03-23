b2a02930d5336999410faa3685b344231bb60119
Ali <aliahadmd3@gmail.com>
2024-03-23 20:17:22 +0000

    redesign website

---
+++ app/utils/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "FinTrac",
+    description:
+      "🏦 Welcome to FinTrac, your all-in-one solution for modern online banking management!",
+    link: "https://github.com/aliahadmd/FinTrac",
+  },
+  {
+    title: "LogisticsLink",
+    description:
+      "Develop a solution that would simplify the process of connecting these stakeholders, facilitating seamless collaboration and optimizing freight contract work.",
+    link: "https://github.com/aliahadmd/LogisticsLink",
+  },
+  {
+    title: "Flowrite",
+    description:
+      "Newsletter based saas app. Build with expressjs, sequelize, tailwindcss, daisyui, sql and others.",
+    link: "https://github.com/aliahadmd/Flowrite",
+  },
+  {
+    title: "NetDevOps-Linux",
+    description:
+      "This repository is dedicated to providing resources, code examples, and tutorials related to computer networking, DevOps practices, and Linux administration.",
+    link: "https://github.com/aliahadmd/NetDevOps-Linux",
+  },
+  {
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    title: "BrainSpark",
+    description:
+      "The Course Platform Project is an innovative web application aimed at revolutionizing online learning experiences. It provides a comprehensive platform for both educators and learners to interact, collaborate, and engage in a dynamic educational environment.",
+    link: "https://github.com/aliahadmd/BrainSpark",
+  },
+];
